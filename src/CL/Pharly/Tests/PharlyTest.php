7f345e12aae1cdad85214415df49cc1047a78034
Cas Leentfaar <git@casleentfaar.com>
2014-07-13 20:10:07 +0200

    fixed tests properly

--- src/CL/Pharly/Tests/PharlyTest.php
+++ src/CL/Pharly/Tests/PharlyTest.php
@@ -32,15 +32,18 @@ class PharlyTest extends \PHPUnit_Framework_TestCase
 
     public function testArchive()
     {
-        $this->createAndTestArchive('zip');
-        $this->createAndTestArchive('tar');
-        $this->createAndTestArchive('tar.gz');
-        $this->createAndTestArchive('tar.bz2');
+        $this->createAndTestArchive('.zip');
+        $this->createAndTestArchive('.tar');
+        $this->createAndTestArchive('.tar.gz');
+        $this->createAndTestArchive('.tar.bz2');
     }
 
     public function testExtract()
     {
-        $this->markTestIncomplete('Not yet...');
+        $this->createAndTestArchiveExtraction('.zip');
+        $this->createAndTestArchiveExtraction('.tar');
+        $this->createAndTestArchiveExtraction('.tar.gz');
+        $this->createAndTestArchiveExtraction('.tar.bz2');
     }
 
     /**
@@ -49,26 +52,84 @@ class PharlyTest extends \PHPUnit_Framework_TestCase
     protected function createAndTestArchive($extension)
     {
         $destination = $this->getPathToArchive($extension);
-        if (file_exists($destination)) {
-            unlink($destination);
-        }
-        $archive     = $this->pharly->archive($destination, [
-            'my/file.txt' => $this->getPathToTestFile(),
-        ]);
+        $archive     = $this->createArchive($extension);
         $this->assertInstanceOf('\PharData', $archive);
-        $this->assertTrue(file_exists($destination));
+        $this->assertFileExists($destination);
         $this->assertContains('Hello World!', $archive['my/file.txt']->getContent());
         unlink($destination);
     }
 
+    protected function createAndTestArchiveExtraction($extension)
+    {
+        $destination = $this->getPathToArchive($extension);
+        $archive     = $this->createArchive($extension);
+        $this->pharly->extract($archive->getPath(), $this->getPathToExtractionDir());
+        $extractedFilePath = $this->getPathToExtractionDir() . 'my/file.txt';
+        $this->assertFileExists($extractedFilePath);
+        $this->assertContains('Hello World!', file_get_contents($extractedFilePath));
+        $this->rmdirRecursive(dirname($extractedFilePath));
+        unlink($destination);
+    }
+
     /**
-     * @param string $extension
+     * Recursively removes a directory that may have been created during extraction
      *
-     * @return string
+     * @param string $path Path to the directory
+     *
+     * @return bool When removal was successful
+     */
+    protected function rmdirRecursive($path)
+    {
+        $files = array_diff(scandir($path), array('.', '..'));
+        foreach ($files as $file) {
+            if (is_dir("$path/$file")) {
+                $this->rmdirRecursive("$path/$file");
+            } else {
+                unlink("$path/$file");
+            }
+        }
+
+        return rmdir($path);
+    }
+
+    /**
+     * Returns a testing location used to extract archives
+     *
+     * @return string The resulting path.
+     */
+    protected function getPathToExtractionDir()
+    {
+        return __DIR__ . '/test_extract/';
+    }
+
+    /**
+     * Creates an archive from a given extension, to be used during tests.
+     *
+     * @param string $extension The extension to create the archive with.
+     *
+     * @return \PharData The created archive.
+     */
+    protected function createArchive($extension)
+    {
+        $destination = $this->getPathToArchive($extension);
+        if (file_exists($destination)) {
+            unlink($destination);
+        }
+        $archive = $this->pharly->archive($destination, [
+            'my/file.txt' => $this->getPathToTestFile(),
+        ]);
+
+        return $archive;
+    }
+
+    /**
+     * @param string $extension The extension to create the path with.
+     *
+     * @return string The resulting path.
      */
     protected function getPathToArchive($extension)
     {
-        return sprintf(__DIR__ . '/test_archive/test_archive.%s', $extension);
+        return sprintf(__DIR__ . '/test_archive/test_archive%s', $extension);
     }
 
     /**
